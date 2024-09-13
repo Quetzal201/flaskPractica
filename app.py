e8fffe8e847b8f7ad8298acb5c7e7e34f51c6ed1
Quetzal201 <166778998+Quetzal201@users.noreply.github.com>
2024-09-12 18:14:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("app.html")
+
+@app.route("/evento")
+def evento()
+    pusher_client = pusher.Pusher(
+      app_id='1767999',
+      key='c41e2f5ebe527dbc13ab',
+      secret='d98385671bdd57829d94',
+      cluster='us2',
+      ssl=True
+    )
+
+    pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
+    
 
 @app.route("/alumnos")
 def alumnos():
